f97008558930980907b20413e84c7e4c66ae779c
Attila Kovács <ati.kovacs98@gmail.com>
2020-10-01 17:43:39 +0200

    (5) Local Flat list and LoadData method

--- irf_4_het_JV6INX/irf_4_het_JV6INX/Form1.cs
+++ irf_4_het_JV6INX/irf_4_het_JV6INX/Form1.cs
@@ -12,9 +12,18 @@ namespace irf_4_het_JV6INX
 {
     public partial class Form1 : Form
     {
+        List<Flat> Flats;
+        RealEstateEntities context = new RealEstateEntities();
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+
+        }
+
+        private void LoadData()
+        {
+            Flats = context.Flats.ToList();
         }
     }
 }
