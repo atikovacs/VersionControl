6781179ceb84124d8ee8b34ea8dfbcfd2001fa85
atikovacs <attila.kovacs6@stud.uni-corvinus.hu>
2020-10-08 15:38:56 +0200

    (5) Portfolio in memory

---
+++ irf_5_het_JV6INX/irf_5_het_JV6INX/Entities/PortfolioItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace irf_5_het_JV6INX.Entities
+{
+    class PortfolioItem
+    {
+        public string Index { get; set; }
+        public decimal Volume { get; set; }
+    }
+}
