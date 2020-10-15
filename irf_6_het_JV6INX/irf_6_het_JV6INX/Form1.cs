0ebbb8ee1aed2c2eb243dbccf561643d204c4143
atikovacs <attila.kovacs6@stud.uni-corvinus.hu>
2020-10-15 17:05:29 +0200

    (6) Displaying Data on  Chart

--- irf_6_het_JV6INX/irf_6_het_JV6INX/Form1.cs
+++ irf_6_het_JV6INX/irf_6_het_JV6INX/Form1.cs
@@ -9,13 +9,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 
 namespace irf_6_het_JV6INX
 {
     public partial class Form1 : Form
     {
-        BindingList<RateData> Rates;
+        BindingList<RateData> Rates = new BindingList<RateData>();
         public Form1()
         {
             InitializeComponent();
@@ -52,6 +53,23 @@ namespace irf_6_het_JV6INX
                 var unit = decimal.Parse(childElement.GetAttribute("unit"));
                 var value = decimal.Parse(childElement.InnerText);
                 if (unit != 0) rate.Value = value / unit;
+
+                //6
+                chartRateData.DataSource = Rates;
+                var series = chartRateData.Series[0];
+
+                series.ChartType = SeriesChartType.Line;
+                series.XValueMember = "Date";
+                series.YValueMembers = "Value";
+                series.BorderWidth = 2;
+
+                var legend = chartRateData.Legends[0];
+                legend.Enabled = false;
+
+                var chartArea = chartRateData.ChartAreas[0];
+                chartArea.AxisX.MajorGrid.Enabled = false;
+                chartArea.AxisY.MajorGrid.Enabled = false;
+                chartArea.AxisY.IsStartedFromZero = false;
             }
 
         }
