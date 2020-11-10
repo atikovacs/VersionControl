51fff21d83b298afd5141966718e94f303b84443
atikovacs <attila.kovacs6@stud.uni-corvinus.hu>
2020-11-10 18:33:32 +0100

    (5) Toy abstract class created

---
+++ irf_8_het_JV6INX/irf_8_het_JV6INX/Abstractions/Toy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace irf_8_het_JV6INX.Abstractions
+{
+    public abstract class Toy : Label
+    {
+        public Toy()
+        {
+            AutoSize = false;
+            Height = 50;
+            Width = 50;
+            Paint += Toy_Paint;
+        }
+        private void Toy_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+
+        protected void DrawImage(Graphics g)
+        {
+            g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
+        }
+
+        public virtual void MoveToy()
+        {
+            Left += 1;
+        }
+    }
+}
