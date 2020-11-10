0512043e592c50c116e6cd65915097258ebae3fc
atikovacs <attila.kovacs6@stud.uni-corvinus.hu>
2020-11-10 19:05:18 +0100

    (8) Created IToyFactory abstract class

---
+++ irf_8_het_JV6INX/irf_8_het_JV6INX/Abstractions/IToyFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace irf_8_het_JV6INX.Abstractions
+{
+    public interface IToyFactory
+    {
+        Toy CreateNew();
+    }
+}
