63d06c7e5cfba030cf5567bdc0ca796202656323
atikovacs <attila.kovacs6@stud.uni-corvinus.hu>
2020-11-10 17:50:49 +0100

    (3) Created BallFactory class

---
+++ irf_8_het_JV6INX/irf_8_het_JV6INX/Entities/BallFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace irf_8_het_JV6INX.Entities
+{
+    class BallFactory
+    {
+        public Ball CreateNew()
+        {
+            return new Ball();
+        }
+    }
+}
