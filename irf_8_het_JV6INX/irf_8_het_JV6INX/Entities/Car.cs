5f032e42adace93e1548d52527d041f8273e13b2
atikovacs <attila.kovacs6@stud.uni-corvinus.hu>
2020-11-10 18:56:46 +0100

    (7) Created Car class

---
+++ irf_8_het_JV6INX/irf_8_het_JV6INX/Entities/Car.cs
@@ -0,0 +1,19 @@
+﻿using irf_8_het_JV6INX.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace irf_8_het_JV6INX.Entities
+{
+    public class Car : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            Image imageFile = Image.FromFile("Images/car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}
