182f45575ec728740f95dc5db994eef5ed1b813b
atikovacs <attila.kovacs6@stud.uni-corvinus.hu>
2020-11-18 15:24:43 +0100

    (6) Validating password

--- UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -12,7 +12,7 @@ namespace UnitTestExample.Test
     {
         [
          Test,
-         TestCase("abc123", false),
+         TestCase("abcd1234", false),
          TestCase("irf@uni-corvinus", false),
          TestCase("irf.uni-corvinus.hu", false),
          TestCase("irf@uni-corvinus.hu", true)
@@ -26,6 +26,27 @@ namespace UnitTestExample.Test
             var actualResult = accountController.ValidateEmail(email);
 
             // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [
+         Test,
+         TestCase("abcdABCD", false),
+         TestCase("ABCD1234", false),
+         TestCase("abcd1234", false),
+         TestCase("Ab1234", false),
+         TestCase("Abcd1234", true)
+        ]
+
+        public void TestValidatePassword(string password, bool expectedResult)
+        {
+            //Arrange
+            var accountController = new AccountController();
+
+            //Act
+            var actualResult = accountController.ValidatePassword(password);
+
+            //Assert
             Assert.AreEqual(expectedResult, actualResult);
         }
     }
