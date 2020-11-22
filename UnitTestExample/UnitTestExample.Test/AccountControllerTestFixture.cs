e375b26ec7e111793623e7a551d28f4a50f7b5bd
atikovacs <attila.kovacs6@stud.uni-corvinus.hu>
2020-11-22 19:24:23 +0100

    (9) Register function HappyPath

--- UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using UnitTestExample.Controllers;
 
@@ -48,6 +49,30 @@ namespace UnitTestExample.Test
 
             //Assert
             Assert.AreEqual(expectedResult, actualResult);
+
+            //Regex
+            //    Regex r = new Regex("^[A-Z0-9]{6}$");
+            //    r.IsMatch(password);
         }
+        [
+        Test,
+        TestCase("irf@uni-corvinus.hu", "Abcd1234"),
+        TestCase("irf@uni-corvinus.hu", "Abcd1234567"),
+        ]
+        public void TestRegisterHappyPath(string email, string password)
+        {
+            // Arrange
+            var accountController = new AccountController();
+
+            // Act
+            var actualResult = accountController.Register(email, password);
+
+            // Assert
+            Assert.AreEqual(email, actualResult.Email);
+            Assert.AreEqual(password, actualResult.Password);
+            Assert.AreNotEqual(Guid.Empty, actualResult.ID);
+        }
+
+
     }
 }
