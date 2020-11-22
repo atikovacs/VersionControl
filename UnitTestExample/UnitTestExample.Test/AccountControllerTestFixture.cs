40947e462079c51c50dca3a5253a9bcb34d477b9
atikovacs <attila.kovacs6@stud.uni-corvinus.hu>
2020-11-22 20:14:30 +0100

    (10) Register function validation exception

--- UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using System;
+using System.Activities;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -54,6 +55,7 @@ namespace UnitTestExample.Test
             //    Regex r = new Regex("^[A-Z0-9]{6}$");
             //    r.IsMatch(password);
         }
+
         [
         Test,
         TestCase("irf@uni-corvinus.hu", "Abcd1234"),
@@ -73,6 +75,33 @@ namespace UnitTestExample.Test
             Assert.AreNotEqual(Guid.Empty, actualResult.ID);
         }
 
+        [
+        Test,
+        TestCase("irf@uni-corvinus", "Abcd1234"),
+        TestCase("irf.uni-corvinus.hu", "Abcd1234"),
+        TestCase("irf@uni-corvinus.hu", "abcd1234"),
+        TestCase("irf@uni-corvinus.hu", "ABCD1234"),
+        TestCase("irf@uni-corvinus.hu", "abcdABCD"),
+        TestCase("irf@uni-corvinus.hu", "Ab1234"),
+        ]
+        public void TestRegisterValidateException(string email, string password)
+        {
+            // Arrange
+            var accountController = new AccountController();
+
+            // Act
+            try
+            {
+                var actualResult = accountController.Register(email, password);
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                Assert.IsInstanceOf<ValidationException>(ex);
+            }
+
+            // Assert
+        }
 
     }
 }
