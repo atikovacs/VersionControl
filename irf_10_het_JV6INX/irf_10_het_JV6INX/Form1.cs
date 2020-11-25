3e1c2c6dfe7a4d1e9dfa0853b821ffe01e54aad0
atikovacs <attila.kovacs6@stud.uni-corvinus.hu>
2020-11-25 13:34:17 +0100

    (3) Testgame Created

--- irf_10_het_JV6INX/irf_10_het_JV6INX/Form1.cs
+++ irf_10_het_JV6INX/irf_10_het_JV6INX/Form1.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 using WorldsHardestGame;
 
 namespace irf_10_het_JV6INX
@@ -20,6 +12,9 @@ namespace irf_10_het_JV6INX
             InitializeComponent();
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
+
+            //gc.AddPlayer();
+            //gc.Start(true);
         }
     }
 }
